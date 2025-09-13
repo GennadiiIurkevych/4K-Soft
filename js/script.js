accf91093aff2e510fe616b7896d67ba91257a89
Gennadii <yurgensport@gmail.com>
2025-09-13 18:31:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,162 @@
+'use strict';
+
+// a = 20;
+// console.log(a);
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//   a: 50
+// };
+
+// obj.a = 15;
+
+// console.log(obj);
+
+// console.log(name);
+// var name = 'Ivan';
+
+// {
+//   const result = 60;
+// }
+
+// // console.log(result);
+
+// alert(5);
+// [0].push('a'); 
+
+// let namber = 4.6;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const persone = "Alex asdtyfhuj rdsfb drfjukl, wdfg";
+// const pers = "5";
+
+// const bool = true; //false
+
+// console.log(something); //null
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//   name: "John",
+//   age: 25,
+//   isMarried: false
+// };
+// console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+// const arr1 = [1, 2, 3];
+
+// console.log(arr1[1]);
+
+// const obj1 = {a: 1, b: 2};
+// const obj = {
+//   Anna: 500, 
+//   'Alice': 800
+// };
+
+// const arr = ['a', 'b', 'c'];
+// arr[10] = '3456';
+
+// console.log(arr[10]);
+// console.log(arr);
+
+// const arrObj = {
+//   a: 'a',
+//   1: 'b',
+//   2: 'c',
+//   abc: {
+//       fed: [{}, {}],
+//       def: {
+
+//       }
+//   }
+// };
+// const d = 'd';
+// // arrObj.d = d;
+// // console.log(arrObj.d);
+// arrObj.b = '1234';
+// arrObj['c'] = '4321';
+// arrObj[d] = '5678';
+// console.log(arrObj.d);
+
+// console.log(arrObj[d]);
+
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+// console.log(arrObj.c);
+// console.log(arrObj['c']);
+// console.log(arrObj.d);
+// console.log(arrObj['d']);
+
+
+// // console.log(arr[1]);
+// // console.log(arrObj.a);
+// console.log(arrObj[1]);
+
+// alert('Hello!');
+
+// confirm("Are you 18?");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+
+// const answer = +prompt("Вам є 18?", "18");
+// console.log(typeof(answer), answer);
+
+// const answer1 = prompt("Вам є 18?", "");
+// console.log(answer1 + 5);
+
+// const answer2 = +prompt("Вам є 18?", "18");
+// console.log(typeof(answer2),answer2 + 5);
+
+// const answer3 = prompt("Вам є 18?", "18");
+// console.log(5 + answer3 );
+
+// const answers = [];
+
+// answers[0] = prompt("Яке ваше ім'я?", "");
+// answers[1] = prompt("Яке ваше прізвище?", "");
+// answers[2] = prompt("Скільки вам років?", "");
+
+// console.log(answers);
+
+// document.write(answers);
+
+// console.log(typeof(answers));
+
+// console.log('arr' + ' - object');
+
+// console.log(4 + ' - object');
+
+// console.log(4 + +' - object');
+// console.log(4 + +'5');
+// console.log(4%2);
+
+
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+// const isChecked = true,
+//       isClose = false;
+
+// console.log(isChecked && isClose);
+// console.log(isChecked || isClose);
